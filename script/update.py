d1c11d02428a1e2c26f9a8385cc1b7137a42fe10
Haibo Z <61728435+ahai-code@users.noreply.github.com>
2024-04-06 11:17:40 +0800

    Update update.py

--- script/update.py
+++ script/update.py
@@ -13,7 +13,7 @@ for file_path in glob.glob(os.path.join(input_dir, '*.json')):
   }
   with open(file_path, 'r') as f:
     data = json.load(f)
-      for item in data:
+    for item in data:
         for k,v in item:
           for vv in v:
             os = vv["Os"]
