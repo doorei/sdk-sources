3b5dedd5529763cc4721470892f23bd9ff9745c9
Haibo Z <61728435+ahai-code@users.noreply.github.com>
2024-04-06 11:16:37 +0800

    Update update.py

--- script/update.py
+++ script/update.py
@@ -7,21 +7,21 @@ output_dir = '../current'
 
 for file_path in glob.glob(os.path.join(input_dir, '*.json')):
   classified_data = {
-            "windows": [],
-            "linux": [],
-            "darwin": []
-        }
-    with open(file_path, 'r') as f:
-        data = json.load(f)
-        for item in data:
-          for k,v in item:
-            for vv in v:
-              os = vv["Os"]
-              if os in classified_data:
-                  del vv["Extra"]
-                  classified_data[os].append(vv)
-    base_name = os.path.basename(file_path)
-    output_file_name = base_name.replace('.version.json', '.json')
-    output_file_path = os.path.join(output_dir, output_file_name)
-    with open(output_file_path, 'w') as f:
-        json.dump(classified_data, f)
+          "windows": [],
+          "linux": [],
+          "darwin": []
+  }
+  with open(file_path, 'r') as f:
+    data = json.load(f)
+      for item in data:
+        for k,v in item:
+          for vv in v:
+            os = vv["Os"]
+            if os in classified_data:
+                del vv["Extra"]
+                classified_data[os].append(vv)
+  base_name = os.path.basename(file_path)
+  output_file_name = base_name.replace('.version.json', '.json')
+  output_file_path = os.path.join(output_dir, output_file_name)
+  with open(output_file_path, 'w') as f:
+    json.dump(classified_data, f)
